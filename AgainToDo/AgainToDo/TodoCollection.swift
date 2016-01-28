30de547a8ace137fc89fb264eb8c6cef01c2db71
TakeumiYamamura <yama1223xxx@gmail.com>
2016-01-28 22:38:26 +0900

    add model

---
+++ AgainToDo/AgainToDo/TodoCollection.swift
@@ -0,0 +1,13 @@
+//
+//  TodoCollection.swift
+//  AgainToDo
+//
+//  Created by TakefumiYamamura on 2016/01/28.
+//  Copyright © 2016年 div. All rights reserved.
+//
+
+import UIKit
+
+class TodoCollection: NSObject {
+
+}
