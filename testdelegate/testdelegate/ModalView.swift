bd2ce02d658f88e49fd6c57ca9b109009800f0d0
TakeumiYamamura <yama1223xxx@gmail.com>
2016-01-29 12:01:45 +0900

    練習用

---
+++ testdelegate/testdelegate/ModalView.swift
@@ -0,0 +1,21 @@
+//
+//  ModalView.swift
+//  testdelegate
+//
+//  Created by TakefumiYamamura on 2016/01/29.
+//  Copyright © 2016年 div. All rights reserved.
+//
+
+import UIKit
+
+class ModalView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
